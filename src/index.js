db178ec499dfee21ebdac6535707d75999df5a39
marcosm <marcos.martilotta@gmail.com>
2022-07-28 23:22:14 -0300

    agregue fondo gris

--- src/index.js
+++ src/index.js
@@ -7,16 +7,28 @@ const linkAleatorio = () => {
 
 const addImg = () => {
     const image = document.createElement('img');
+    image.className = 'newFox'; 
     image.dataset.src = linkAleatorio();
     const container = document.querySelector('#images');
     container.append(image);
     registerImage(image);
+    
 }
 
+const clean = (cleaner) => {
+    const container = document.querySelector('#images');
+    const image = document.querySelectorAll('img'); //obtengo un nodeList
+    const imageArray = [...image];
+   /*  debugger; */
+    imageArray.forEach(element => container.removeChild(element));
+    
+}
 
 const button = document.querySelector('#add');
-
 button.addEventListener('click', addImg);
+
+const cleaner = document.querySelector('#clean');
+cleaner.addEventListener('click', clean);
 
 
 
