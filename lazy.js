68b7ba2968c71d7156c359f8827c537b14a6fc8c
marcosm <marcos.martilotta@gmail.com>
2022-07-29 20:19:27 -0300

    Comiteo bien

---
+++ lazy.js
@@ -0,0 +1,33 @@
+//
+/* import { printTotal } from './index.js' */
+/* let appendImages = 0;
+let loadImages = 0; */
+
+const isIntersecting = (entry) => {
+    return entry.isIntersecting; //true dentro de la pantalla
+}
+
+const loadImage = (entry) => {
+    //const container = entry.target; //aca tengo el contenedor, el div
+    const imagen = entry.target; //Ya se que dentro de el div el primer hijo es la imagen
+    //debugger
+    const url = imagen.dataset.src; //Dentro de data en src tengo la urlque le comunique con js
+    //debugger;
+
+    //Desrregistra dentro de la pantalla
+    imagen.src = url; //ahora si, pongo en el src de mi etiqueta imgan en la url de la imagen;
+    observer.unobserve(imagen)
+    loadImages ++;
+    printTotal(appendImages, loadImages);
+
+};
+
+//MAs info de la api del viewport en MDN 
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(isIntersecting).forEach(loadImage);
+
+})
+
+export const registerImage = (imagen) => {
+    observer.observe(imagen);
+}
